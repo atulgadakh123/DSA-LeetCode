7108faa700d50bcc6b26e25df7619729efd63c1d
Atul Gadakh <121969217+atulgadakh123@users.noreply.github.com>
2025-08-21 14:35:34 +0530

    Time: 4 ms (36.76%) | Memory: 43.3 MB (72.97%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,26 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates); // important for skipping duplicates
-        List<List<Integer>> ans = new ArrayList<>();
-        backtrack(candidates, 0, target, new ArrayList<>(), ans);
-        return ans;
-    }
-
-    private void backtrack(int[] arr, int idx, int remain, List<Integer> path, List<List<Integer>> ans) {
-        if (remain == 0) {
+      Arrays.sort(candidates);
+       List<List<Integer>>ans=new ArrayList<>();
+       backtrack(candidates,0,target,new ArrayList<>(),ans);
+       return ans;
+  }
+       public void backtrack(int[] arr, int start,int target,List<Integer>path,List<List<Integer>>ans){
+         if(target==0){
             ans.add(new ArrayList<>(path));
             return;
+         }
+         if(target<=0 || start==arr.length)
+         return ;
+
+         path.add(arr[start]);
+         backtrack(arr,start+1, target-arr[start],path,ans);
+         path.remove(path.size()-1);
+       //  backtrack(arr,start-path,ans);
+       //skip arr[start] and skip all duplicate
+       int next=start+1;
+       while(next<arr.length && arr[next]==arr[start]){
+        next++;
         }
-        if (remain < 0 || idx == arr.length) return;
-
-        // Option 1: take arr[idx]
-        path.add(arr[idx]);
-        backtrack(arr, idx + 1, remain - arr[idx], path, ans);
-        path.remove(path.size() - 1);
-
-        // Option 2: skip arr[idx] and skip all duplicates
-        int next = idx + 1;
-        while (next < arr.length && arr[next] == arr[idx]) next++;
-        backtrack(arr, next, remain, path, ans);
-    }
-}
+        backtrack(arr,next,target,path,ans);
+       }
+       }
+  
