02520621f01125412dbb29455cca972b99d2fb81
Atul Gadakh <121969217+atulgadakh123@users.noreply.github.com>
2025-08-30 07:12:06 +0530

    Time: 3 ms (39.73%) | Memory: 42.2 MB (13.47%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()<1){
+            return false;
+        }
+
+        Stack <Character>stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+           char ch=s.charAt(i);
+
+           if(ch== '{' || ch == '[' || ch == '(' ){
+            stack.push(ch);
+           }
+            else if(stack.empty()){
+                return false;
+            }
+
+            else{
+                if(ch==']'){
+                   if(stack.pop() != '['){
+                    return false;
+                    }
+                }
+                    if(ch==')'){
+                   if(stack.pop() != '('){
+                    return false;
+                    }
+                }
+                    if(ch=='}'){
+                   if(stack.pop() != '{'){
+                    return false;
+                    }
+                }
+                
+                
+           
+           }
+        }
+       
+         return stack.empty();
+    }
+}
